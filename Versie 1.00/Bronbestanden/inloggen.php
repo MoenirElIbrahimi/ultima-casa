096f5f7e572f7f9f9d878a3bcd9f8f5b25056682
Void787 <1195841@student.roc-nijmegen.nl>
2024-01-24 14:21:28 +0100

    3. updaten, gebleven bij koopplus

--- Versie 1.00/Bronbestanden/inloggen.php
+++ Versie 1.00/Bronbestanden/inloggen.php
@@ -1,27 +1,33 @@
 <?php
 
-     include_once("functions.php");
-     
-     $email = '"' . $_GET["Email"] . '"'; 
-     $ww = '"' . md5($_GET["Wachtwoord"]) . '"'; 
-     
-     $db = ConnectDB();
-     $sql = "   SELECT relaties.ID as RID,
-                       rollen.Waarde as Rol,
-                       Landingspagina 
-                  FROM relaties
-             LEFT JOIN rollen
-                    ON relaties.FKrollenID = rollen.ID
-                 WHERE (Email = $email) 
-                   AND (Wachtwoord = $ww)";
-                   
-     $inlog = $db->query($sql)->fetch();
-     
-     $redirect_url = 'index.php?NOAccount';
-     if ($inlog)
-     {    $redirect_url = $inlog['Landingspagina'] . '?RID=' . $inlog['RID'];
-     }
-     
-     echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '. $redirect_url . '">';
-     
+include_once("functions.php");
+
+// Controleer of de vereiste variabelen zijn ingesteld
+if (isset($_POST["Email"]) && isset($_POST["Wachtwoord"])) {
+    $email = $_POST["Email"];
+    $ww = md5($_POST["Wachtwoord"]);
+
+    $db = ConnectDB();
+    $sql = "SELECT relaties.ID as RID,
+                   rollen.Waarde as Rol,
+                   Landingspagina 
+              FROM relaties
+         LEFT JOIN rollen
+                ON relaties.FKrollenID = rollen.ID
+             WHERE (Email = '$email') 
+               AND (Wachtwoord = '$ww')";
+
+    $inlog = $db->query($sql)->fetch();
+
+    $redirect_url = 'index.php?NOAccount';
+    if ($inlog) {
+        $redirect_url = $inlog['Landingspagina'] . '?RID=' . $inlog['RID'];
+    }
+
+    echo '<META HTTP-EQUIV=REFRESH CONTENT="1; ' . $redirect_url . '">';
+} else {
+    // Als Email of Wachtwoord niet is ingesteld, doe hier iets (bijvoorbeeld geef een foutmelding weer).
+    echo "Email en/of Wachtwoord niet ingesteld.";
+}
+
 ?>
