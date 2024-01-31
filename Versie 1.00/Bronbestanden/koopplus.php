b65dd1c1064cc4a04caa5522c1713d87ff49bc83
Void787 <1195841@student.roc-nijmegen.nl>
2024-01-31 13:34:59 +0100

    gebleven bij reletieupd

--- Versie 1.00/Bronbestanden/koopplus.php
+++ Versie 1.00/Bronbestanden/koopplus.php
@@ -3,45 +3,44 @@ include_once("functions.php");
 
 $db = ConnectDB();
 
-$relatieid = $_GET['RID'];
+$relatieid = $_POST['RID'];
 
 $filter = 0;
 $filterids = "";
 
-if (isset($_GET['filter'])) {
+if (isset($_POST['filter'])) {
     $filter = 1;
     $sql = "SELECT FKhuizenID AS huizenID
-            FROM mijncriteria
+            FROM mijncriteria 
             LEFT JOIN huiscriteria ON huiscriteria.FKcriteriaID = mijncriteria.FKcriteriaID
             LEFT JOIN criteria ON criteria.ID = mijncriteria.FKcriteriaID
-            WHERE (mijncriteria.FKrelatiesID = $relatieid)
+            WHERE (mijncriteria.FKrelatiesID = $relatieid) 
             GROUP BY huizenID
             HAVING SUM(IF(criteria.Type < 2, huiscriteria.Waarde BETWEEN mijncriteria.Van AND mijncriteria.Tem, mijncriteria.Van = huiscriteria.Waarde)) = COUNT(*)";
     $fids = $db->query($sql)->fetchAll();
-
+    
     $filterids = array(-1);
     foreach ($fids as $filterid) {
         $filterids[] = $filterid["huizenID"];
     }
     $filterids = "(huizen.ID IN (" . implode(",", $filterids) . ")) AND ";
-};
+}
 
 $sql = "SELECT huizen.ID AS HID,
                StartDatum,
                Straat,
                CONCAT(LEFT(Postcode, 4), ' ', RIGHT(Postcode, 2), ', ', Plaats) as Plaats
-        FROM huizen
+        FROM huizen 
         LEFT JOIN biedingen ON biedingen.FKhuizenID = huizen.ID
         LEFT JOIN statussen ON statussen.ID = biedingen.FKstatussenID
         WHERE $filterids (IFNULL(StatusCode, 0) < 10) AND (huizen.ID NOT IN (SELECT FKhuizenID FROM biedingen WHERE FKrelatiesID = 673))
-        GROUP BY huizen.ID";
+        GROUP BY huizen.ID"; 
 
 $kopen = $db->query($sql)->fetchAll();
 ?>
 
 <!DOCTYPE html>
 <html lang="nl">
-
 <head>
     <title>Mijn Ultima Casa</title>
     <meta charset="utf-8">
@@ -51,74 +50,88 @@ $kopen = $db->query($sql)->fetchAll();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="ucstyle.css?<?= mt_rand() ?>">
 </head>
-
 <body>
     <div class="container">
-        <form action="koopins.php">
+        <form action="koopins.php" method="POST">   
             <table id="mijnkopen" class="koop">
                 <tr>
                     <th colspan=3>
                         <h4>
-                            <?php if ($filter > 0) {
-                                echo 'Geselecteerde';
-                            } else {
-                                echo 'Alle';
-                            }; ?> beschikbare huizen
+                            <?php echo ($filter > 0) ? 'Geselecteerde' : 'Alle'; ?> beschikbare huizen
                         </h4>
                     </th>
                     <th class="button-column">
                         <button class="action-button" disabled>
-                            <?php if ($filter > 0) {
-                                echo '<a href="koopplus.php?RID=' . $relatieid . '" title="Alle beschikbare huizen tonen.">&#x2610;</a>';
-                            } else {
-                                echo '<a href="koopplus.php?RID=' . $relatieid . '&filter" title="Alleen geselecteerde huizen tonen.">&#x2611;</a>';
-                            } ?>
+                            <?php echo ($filter > 0) ? '<a href="koopplus.php?RID=' . $relatieid . '" title="Alle beschikbare huizen tonen.">&#x2610;</a>' : '<a href="koopplus.php?RID=' . $relatieid . '&filter" title="Alleen geselecteerde huizen tonen.">&#x2611;</a>'; ?>
                         </button>
                     </th>
-                    <th><button class="action-button"><a href="relatie.php?RID=<?= $relatieid ?>" >Annuleren</a></button>
+                    <th>
+                        <button class="action-button">
+                            <a href="relatie.php?RID=<?php echo $relatieid; ?>" >Annuleren</a>
+                        </button>
                     </th>
                 </tr>
-                <?php
-                if (count($kopen) > 0) {
-                    echo
-                        '<tr>
-                            <th>Te koop sinds</th>
-                            <th>Straat</th>
-                            <th>Plaats</th>
-                            <th>&nbsp;</th>
-                            <th>&nbsp;</th>
-                        </tr>';
-                    foreach ($kopen as $tekoop) {
-                        $sql = "SELECT IF(TYPE=1, Waarde, IF(Waarde > 0, 'Ja', 'Nee')) AS Waarde, Criterium, Type
+
+                <?php if (count($kopen) > 0): ?>
+                    <tr>
+                        <th>Te koop sinds</th>
+                        <th>Straat</th>
+                        <th>Plaats</th>
+                        <th>&nbsp;</th>
+                        <th>&nbsp;</th>
+                    </tr>
+
+                    <?php foreach ($kopen as $tekoop): ?>
+                        <?php
+                        $sql = "SELECT IF(TYPE=1, Waarde, IF(Waarde > 0, 'Ja', 'Nee')) AS Waarde, Criterium, Type   
                                 FROM huiscriteria
                                 LEFT JOIN criteria ON criteria.ID = huiscriteria.FKcriteriaID
                                 WHERE huiscriteria.FKhuizenID = " . $tekoop['HID'] . "
                                 ORDER BY Volgorde";
-
+                        
                         $criteria = $db->query($sql)->fetchAll();
+                        ?>
 
-                        echo
-                            '<tr>
-                                <td>' . $tekoop['StartDatum'] . '</td>
-                                <td>' . $tekoop['Straat'] . '</td>
-                                <td>' . $tekoop['Plaats'] . '</td>
-                                <td>';
-                        $disabled = " ";
-                        $details = "Details.";
-                        if (count($criteria) < 1) {
-                            $disabled = " disabled";
-                            $details = "Er zijn geen details beschikbaar.";
-                        }
-                        echo
-                            '<button type="button" class="action-button"' . $disabled . '
-                                    data-toggle="collapse" data-target="#acc_' . $tekoop['HID'] . '" 
-                                    title="' . $details . '">&nbsp;&#9660;&nbsp;
-                            </button>';
-                        echo
-                            '</td>
-                                <td class="button-column">
-                                    <button type="submit" class="action-button" id="plus" name="plus" 
-                                            value="' . $tekoop['HID'] . '" title="Dit huis toevoegen aan mijn lijst.">+</button>
-                                </td>
-                            </tr>
-                            <
+                        <tr>
+                            <td><?php echo $tekoop['StartDatum']; ?></td>
+                            <td><?php echo $tekoop['Straat']; ?></td>
+                            <td><?php echo $tekoop['Plaats']; ?></td>
+                            <td>
+                                <button type="button" class="action-button" <?php echo (count($criteria) < 1) ? 'disabled' : ''; ?>
+                                    data-toggle="collapse" data-target="#acc_<?php echo $tekoop['HID']; ?>" 
+                                    title="<?php echo (count($criteria) < 1) ? 'Er zijn geen details beschikbaar.' : 'Details.'; ?>">&nbsp;&#9660;&nbsp;
+                                </button>
+                            </td>
+                            <td class="button-column">
+                                <button type="submit" class="action-button" id="plus" name="plus" 
+                                        value="<?php echo $tekoop['HID']; ?>" title="Dit huis toevoegen aan mijn lijst.">+</button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colspan=5>
+                                <div id="acc_<?php echo $tekoop['HID']; ?>" class="collapse">
+                                    <div class="form-inline">
+                                        <?php foreach ($criteria as $criterium): ?>
+                                            <div class="form-group spacer-right">
+                                                <?php echo $criterium["Criterium"] . ': <label>' . $criterium["Waarde"] .  '</label>'; ?>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan=5>
+                            Er staan geen huizen te koop die voldoen aan je zoekcriteria
+                        </td>
+                    </tr>
+                <?php endif; ?>
+            </table>
+            <input type="hidden" value="<?php echo $relatieid; ?>" id="RID" name="RID">
+        </form>
+    </div>
+</body>
+</html>
